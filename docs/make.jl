d681c7fad3dbddaa708391b0afef1495c7dde20d
Matt <mattwillflood@gmail.com>
2021-12-31 03:37:35 +0100

    So all previous versions seem to be gone now...
    Go back to original args for versions

--- docs/make.jl
+++ docs/make.jl
@@ -42,7 +42,7 @@ makedocs(
 
 deploydocs(
     repo="github.com/MattWillFlood/EntropyHub.jl.git",
-    versions = ["stable" => "v^", "v#.#"],
+    versions = ["stable" => "v^", "v#.#.#"],
     #versions = nothing,
 )
 
